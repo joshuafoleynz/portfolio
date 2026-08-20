37932c529b8830e7209a41ff583c11b25541fc08
Joshua Foley <joshua.foley@outlook.com>
2026-08-20 19:27:07 +1200

    Build return for App function

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,11 @@
 // Import Outlet
 import { Outlet } from 'react-router'
 
-function App() {}
+function App() {
+  return (
+    <div className="App">
+      <h1>Portfolio</h1>
+      <Outlet />
+    </div>
+  )
+}
